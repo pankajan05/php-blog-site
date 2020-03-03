6b4a8f4c1774cc540edf7315cd8b83a0e0ed9c8e
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-03 11:29:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# php-blog-site
+ This is the php web site for maintain the blogs
---
+++ data.php
@@ -0,0 +1,20 @@
+<?php
+
+$menus = [
+    ["title" => "Home", "url" => "/"],
+    ["title" => "About", "url" => "#about"],
+    ["title" => "Gallery", "url" => "#gallery"],
+    ["title" => "Contact", "url" => "#contact"]
+];
+
+$posts = [
+    [
+        "title" => "Demo title 1",
+        "intro" => "This is simple content and its cool"
+    ],
+    [
+        "title" => "Demo title 2",
+        "intro" => "This is simple content as second article and its cool"
+    ]
+];
+?>
