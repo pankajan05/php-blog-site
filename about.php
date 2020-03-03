37d042f0522aad5039bed80f86673548736112d5
Pankajan Satkunam <pankajan05@gmail.com>
2020-03-03 12:33:59 +0530

    finished the website

--- about.php
+++ about.php
@@ -2,7 +2,17 @@
 include("helper.php");
 include("header.php");
 ?>
+<div class="container">
+    <h2 class="text-center m-5">About</h2>
+    <div class="media p-5 m-5" style="border:0.5px  #5a5a5a solid;">
+        <div class="media-body">
+            <h5 class="mt-0">Sabi blogs</h5>
+            <p>Here we can manage the blogs using the  php very easily. This is php blog maintain website.</p>
+            <p> I design this for get experience in design a project in php.I am also interested in gardening. If you like to create like this project then you can contact me through this number in contact page.</p>
 
+        </div>
+    </div>
+</div>
 
 <?php
 include('footer.php');
--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ include("header.php");
         <div class="media-body">
             <h5 class="mt-0">Pankajan Satkunam</h5>
             <p>I am a software Engineer. I am interested in designing angular project.</p>
-            <p> I design this for get experience in design a project in angular.I am also interested in gardening. If you like to create like this project then you can contact me. through this number.</p>
+            <p> I design this for get experience in design a project in php.I am also interested in gardening. If you like to create like this project then you can contact me. through this number.</p>
             <p><span href="#" class="font-weight-bold">Telephone Number :</span><span class="ml-5 text-right">+94772958306</span></p>
             <p><span href="#" class="font-weight-bold">Email Address :</span><span class="ml-5 text-right">pankajan05@gmail.com</span></p>
             <p><span href="#" class="font-weight-bold">Address: </span><span class="ml-5 p-5 text-right">Earlalai East, Earlalai, Jaffna.</span></p>
